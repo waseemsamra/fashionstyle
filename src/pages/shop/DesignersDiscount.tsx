2bb153dbe3d1d09d27814a07eebc71887044c60d
waseemsamra <waseemsamra@gmail.com>
2026-04-20 10:43:07 +0400

    designer update

--- src/pages/shop/DesignersDiscount.tsx
+++ src/pages/shop/DesignersDiscount.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ShoppingBag, ChevronLeft } from 'lucide-react';
 import { getProductUrl } from '@/utils/productUrl';
