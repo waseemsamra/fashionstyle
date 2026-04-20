693d1e1e34c4f9f422026d9ba26bf69c67702ff4
waseemsamra <waseemsamra@gmail.com>
2026-04-20 11:41:15 +0400

    Make dramatic grid layout change to ensure Designers Discount cards are larger

--- src/pages/shop/DesignersDiscount.tsx
+++ src/pages/shop/DesignersDiscount.tsx
@@ -70,7 +70,7 @@ export default function DesignersDiscount() {
             <p className="text-gray-500">Check back later for new arrivals</p>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8">
             {products.map((product) => (
               <div
                 key={product.id}
