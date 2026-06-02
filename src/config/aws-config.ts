fa1c9c32afdfe3ef2bd7a406657d0c8c89c33393
waseemsamra <waseemsamra@gmail.com>
2026-06-02 17:48:42 +0400

    Remove identity pool - using API key auth for AppSync

--- src/config/aws-config.ts
+++ src/config/aws-config.ts
@@ -6,7 +6,6 @@ Amplify.configure({
     Cognito: {
       userPoolId: import.meta.env.VITE_USER_POOL_ID || '',
       userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID || '',
-      identityPoolId: import.meta.env.VITE_IDENTITY_POOL_ID || '',
     }
   },
   API: {
