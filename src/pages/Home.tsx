bf5b6f0e51b975bd2e0703b5d12e7f2559c781ed
waseemsamra <waseemsamra@gmail.com>
2026-06-06 01:49:14 +0400

    Add Designers Spotlight homepage section and /designers-spotlight page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ import DesignersOnDiscount from '@/components/sections/DesignersOnDiscount';
 import SummerCollection from '@/components/sections/SummerCollection';
 import TrendingNow from '@/components/sections/TrendingNow';
 import LeadingBrands from '@/components/sections/LeadingBrands';
+import DesignersSpotlight from '@/components/sections/DesignersSpotlight';
 import About from '@/components/sections/About';
 import Testimonials from '@/components/sections/Testimonials';
 import Newsletter from '@/components/sections/Newsletter';
@@ -25,6 +26,7 @@ export default function Home() {
       <SummerCollection />
       <TrendingNow />
       <LeadingBrands />
+      <DesignersSpotlight />
       <About />
       <Testimonials />
       <Newsletter />
